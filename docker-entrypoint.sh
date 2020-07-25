c6cfcf45e09111ebfdc545bbcdb0b22fd5a206e0
Maxim Peten <mpeten@mindk.com>
2020-07-25 22:20:47 +0300

    update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ adduser --disabled-password --gecos "" "${SSH_USER}" \
 	&& mkdir "/home/${SSH_USER}/.ssh/" \
 	&& echo "${SSH_KEY}" >> "/home/${SSH_USER}/.ssh/authorized_keys" \
 	&& cp "/run/secrets/${DOCKER_HOST_KEY}" "/home/${SSH_USER}/.ssh/" \
-	&& chmod 600 "/home/${SSH_USER}/${DOCKER_HOST_KEY}" \
+	&& chmod 600 "/home/${SSH_USER}/.ssh/${DOCKER_HOST_KEY}" \
 	&& mv "/db_backup-env-new.sh" "/home/${SSH_USER}/" \
 	&& env | grep _ >> /etc/environment \
 	&& chown -R "${SSH_USER}":"${SSH_USER}" "/home/${SSH_USER}" "/opt/"
